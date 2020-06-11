f2b6732b4957dc7bdd67d562d2f4204bc78161d1
Antoine Raulin <25278917+antoineraulin@users.noreply.github.com>
2020-06-11 16:04:54 +0200

    Initial Commit

---
+++ Haptics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Haptics
+//
+//  Created by Antoine Raulin on 11/06/2020.
+//  Copyright © 2020 Antoine Raulin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
